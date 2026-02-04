a395d6027341b013eea00d28c333110017e75e31
Rodrigo Mascarenhas <rodrigo.h.mascarenhas@gmail.com>
2026-02-03 21:54:56 -0600

    fix: define PORT for Easypanel App

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const PORT = process.env.PORT || 80;
 
 const app = express();
 app.use(express.json());
